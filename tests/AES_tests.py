e465e2ec0369ca0cb2140235a1f2b7fda4f70965
Jordan Collocott <jrdn.collocott@gmail.com>
2024-05-31 13:07:40 +0200

    test: add basic tests for shift_rows, mix_columns
    basic functionality tests, no exhaustive function tests

---
+++ tests/AES_tests.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+from jCrypt import *
+
+
+state = np.array([0x115599DD,
+                  0x2266AAEE,
+                  0x3377BBFF,
+                  0x4488CC11])
+
+
+# Test AES shift_rows function
+test = AES.shift_rows(state)
+
+for i in range(len(test)):
+    print(hex(test[i]))
+print()
+
+
+
+state = np.array([0xD4BF5D30,
+                  0xD4BF5D30,
+                  0xD4BF5D30,
+                  0xD4BF5D30])
+
+# Test AES mix_columns function
+test = AES.mix_columns(state)
+
+for i in range(len(test)):
+    print(hex(test[i]))
+print()
